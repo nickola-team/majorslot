22bc0a8902db6a16cb6b7162a5ebf750deb43eb3
goldenbull <goldenbull@goldenbull.com>
2021-03-30 11:21:07 +0900

    fixed resize issue of pp game blade

--- resources/views/frontend/Default/games/pragmatic.blade.php
+++ resources/views/frontend/Default/games/pragmatic.blade.php
@@ -1,14 +1,37 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <style>
+        body {
+        margin: 0;
+        }
+    </style>
+    <script>
+        window.onload = function () {
+            document.documentElement.style.width = "100%";
+            document.documentElement.style.height = "100%";
+            document.documentElement.style.overflow = 'hidden';
+            document.body.style.width = "100%";
+            document.body.style.height = "100%";
+            var viewport = document.querySelector('meta[name=viewport]');
+            if (!viewport) {
+                var metaTag = document.createElement('meta');
+                metaTag.name = 'viewport';
+                metaTag.content = 'width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no';
+                document.getElementsByTagName('head')[0].appendChild(metaTag);
+            }
+            else {
+                viewport.setAttribute('content', 'width=device-width, height=device-height, initial-scale=1,maximum-scale=1, user-scalable=no');
+            }
+        };
+    </script>
+
     </head>
-    <body style = "
-        margin: 0px;
-        padding: 0px;
-        overflow-y: hidden;
-        overflow-x: hidden;">
-        <iframe style="width: 1280px; height: 720px; border: none;" frameborder="0" border="0" cellspacing="0"
+    <body>
+    <div style="position:relative; width:100%; height:100%;">
+        <iframe id="embedgameIframe" style="margin:0; padding:0; white-space: nowrap; border: 0; width:100%;height:100%;" frameborder="0" border="0" cellspacing="0"
         src=" {{ $url }}"></iframe>
+    </div>
     </body>
 </html>
