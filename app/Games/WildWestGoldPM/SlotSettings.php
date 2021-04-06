06240201124b46d9873beadd7120e3ad30e11623
goldenbull <goldenbull@goldenbull.com>
2021-04-06 10:59:18 +0900

    apply happyhour at new game

--- app/Games/WildWestGoldPM/SlotSettings.php
+++ app/Games/WildWestGoldPM/SlotSettings.php
@@ -64,6 +64,7 @@ namespace VanguardLTE\Games\WildWestGoldPM
         public $doubleWildChance = null;
         public $scatterODSymbol = [];
         public $wildODSymbol = [];
+        public $happyhouruser = null;
         public function __construct($sid, $playerId, $credits = null)
         {
            /* if( config('LicenseDK.APL_INCLUDE_KEY_CONFIG') != 'wi9qydosuimsnls5zoe5q298evkhim0ughx1w16qybs2fhlcpn' ) 
@@ -89,6 +90,11 @@ namespace VanguardLTE\Games\WildWestGoldPM
             $this->playerId = $playerId;
             $this->credits = $credits;
             $user = \VanguardLTE\User::lockForUpdate()->find($this->playerId);
+            $this->happyhouruser = \VanguardLTE\HappyHourUser::where([
+                'user_id' => $user->id, 
+                'status' => 1,
+                'time' => date('G')
+            ])->first();
             $user->balance = $credits != null ? $credits : $user->balance;
             $this->user = $user;
             $this->shop_id = $user->shop_id;
@@ -452,6 +458,11 @@ namespace VanguardLTE\Games\WildWestGoldPM
         }
         public function GetBank($slotState = '')
         {
+            if ($this->happyhouruser)
+            {
+                $this->Bank = $this->happyhouruser->current_bank;
+                return $this->Bank / $this->CurrentDenom;
+            }
             if( $this->isBonusStart || $slotState == 'bonus' || $slotState == 'freespin' || $slotState == 'respin' ) 
             {
                 $slotState = 'bonus';
@@ -561,13 +572,21 @@ namespace VanguardLTE\Games\WildWestGoldPM
             {
                 $this->toGameBanks = $sum;
             }
-            if( $_obf_bonus_systemmoney > 0 ) 
+            if ($this->happyhouruser)
             {
-                $sum -= $_obf_bonus_systemmoney;
-                $game->set_gamebank($_obf_bonus_systemmoney, 'inc', 'bonus');
+                $this->happyhouruser->increment('current_bank', $sum);
+                $this->happyhouruser->save();
             }
-            $game->set_gamebank($sum, 'inc', $slotState);
-            $game->save();
+            else
+            {
+                if( $_obf_bonus_systemmoney > 0 ) 
+                {
+                    $sum -= $_obf_bonus_systemmoney;
+                    $game->set_gamebank($_obf_bonus_systemmoney, 'inc', 'bonus');
+                }
+                $game->set_gamebank($sum, 'inc', $slotState);
+                $game->save();
+            }
             return $game;
         }
         public function SetBalance($sum, $slotEvent = '')
@@ -800,6 +819,34 @@ namespace VanguardLTE\Games\WildWestGoldPM
             $game->{'garant_win' . $_obf_granttype . $_obf_linecount} = $_obf_grantwin_count;
             $game->{'garant_bonus' . $_obf_granttype . $_obf_linecount} = $_obf_grantbonus_count;
             $game->save();
+            if ($this->happyhouruser)
+            {
+                $bonus_spin = rand(1, 10);
+                $bonus_percent = 2;
+                $spin_percent = 9;
+                if ($this->happyhouruser->current_bank <= 10000)
+                {
+                    $bonus_percent = 1;
+                }
+                if ($garantType == 'freespin')
+                {
+                    $bonus_percent = 1;
+                }
+                if ($bonus_spin <= $bonus_percent) {
+                    $bonusWin = 1;
+                    $spinWin = 0;
+                }
+                else if ($bonus_spin < $spin_percent)
+                {
+                    $bonusWin = 0;
+                    $spinWin = 1;
+                }
+                else
+                {
+                    $bonusWin = 0;
+                    $spinWin = 0;
+                }
+            }
             if( $bonusWin == 1 && $this->slotBonus ) 
             {
                 $this->isBonusStart = true;
