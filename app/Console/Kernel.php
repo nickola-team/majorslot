df3bc6c0f83b37afe769d67ba9517c9e281a1cc7
phoo <phoo71719@gmail.com>
2021-05-26 14:14:12 +0800

    update db remove time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,11 +31,15 @@ namespace VanguardLTE\Console
                 $start_date = date("Y-m-d H:i:s",strtotime("-7 days"));
                 \VanguardLTE\GameLog::where('time', '<', $start_date)->delete();
 
+                $start_date = date("Y-m-d H:i:s",strtotime("-10 days"));
+
+                \VanguardLTE\StatGame::where('date_time', '<', $start_date)->delete();
+                \VanguardLTE\DealLog::where('date_time', '<', $start_date)->delete();
+
                 $start_date = date("Y-m-d H:i:s",strtotime("-30 days"));
                 \VanguardLTE\ShopStat::where('date_time', '<', $start_date)->delete();
                 \VanguardLTE\Transaction::where('created_at', '<', $start_date)->delete();
-                \VanguardLTE\StatGame::where('date_time', '<', $start_date)->delete();
-                \VanguardLTE\DealLog::where('date_time', '<', $start_date)->delete();
+                \VanguardLTE\WithdrawDeposit::where('created_at', '<', $start_date)->delete();
 
                 \VanguardLTE\Http\Controllers\Web\GameProviders\PPController::syncpromo();
 
