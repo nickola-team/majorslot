a3ca035937349d8406d407dce1111a279766d7f7
phoo <phoo71719@gmail.com>
2021-05-28 22:47:22 +0800

    unlogged message

--- app/Games/MustangGoldPM/Server.php
+++ app/Games/MustangGoldPM/Server.php
@@ -30,7 +30,7 @@ namespace VanguardLTE\Games\MustangGoldPM
             // $userId = \Auth::id();// changed by game developer
             if( $userId == null ) 
             {
-            	$response = '{"responseEvent":"error","responseType":"","serverResponse":"invalid login"}';
+            	$response = 'unlogged';
                 exit( $response );
             }
             $user = \VanguardLTE\User::lockForUpdate()->find($userId);
