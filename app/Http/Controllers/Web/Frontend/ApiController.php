4772c6dd180acf9a954612eee9f225aebf45bf8b
PHOO <phoo@gmail.com>
2021-06-18 08:39:06 +0800

    enable cq9 games

--- app/Http/Controllers/Web/Frontend/ApiController.php
+++ app/Http/Controllers/Web/Frontend/ApiController.php
@@ -267,14 +267,7 @@ namespace VanguardLTE\Http\Controllers\Web\Frontend
             $categories = [$cat1->id];
             if ($cat1->provider != null)
             {
-                if ($cat1->provider == 'cq9')
-                {
-                    $selectedGames = [];
-                }
-                else
-                {
-                    $selectedGames = $this->gamelistbyProvider($cat1->provider, $cat1->href);
-                }
+                $selectedGames = $this->gamelistbyProvider($cat1->provider, $cat1->href);
             }
             else{
                 if (str_contains(\Illuminate\Support\Facades\Auth::user()->username, 'testfor')) // test account for game providers
