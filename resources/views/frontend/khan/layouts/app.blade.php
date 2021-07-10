cd751205b66a65423046e8ad1a3b921c924260db
PHOO <phoo@gmail.com>
2021-07-10 12:27:52 +0800

    update khan design

--- resources/views/frontend/khan/layouts/app.blade.php
+++ resources/views/frontend/khan/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>@yield('page-title') - {{ settings('app_name') }}</title>
+    <title>@yield('page-title')}}</title>
     <meta name="viewport" content="width=device-width">
     <link rel="icon" href="/frontend/Default/img/favicon.png" >
 
