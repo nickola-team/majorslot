c901447130b6a6808e381b07951804807020856f
PHOO <phoo@gmail.com>
2021-08-02 14:50:34 +0800

    applied happyuser at olympus

--- app/Games/GatesofOlympusPM/SlotSettings.php
+++ app/Games/GatesofOlympusPM/SlotSettings.php
@@ -98,7 +98,7 @@ namespace VanguardLTE\Games\GatesofOlympusPM
         public $doubleWildChance = null;
         public $reelPositionMap = [];
 
-        // public $happyhouruser = null;
+        public $happyhouruser = null;
 
         /* 프리스핀 관련 */
         public $freeSpinTable = [];
@@ -110,11 +110,11 @@ namespace VanguardLTE\Games\GatesofOlympusPM
             $this->playerId = $playerId;
             $this->credits = $credits;
             $user = \VanguardLTE\User::lockForUpdate()->find($this->playerId);
-            // $this->happyhouruser = \VanguardLTE\HappyHourUser::where([
-            //     'user_id' => $user->id, 
-            //     'status' => 1,
-            //     'time' => date('G')
-            // ])->first();
+            $this->happyhouruser = \VanguardLTE\HappyHourUser::where([
+                'user_id' => $user->id, 
+                'status' => 1,
+                'time' => date('G')
+            ])->first();
             $user->balance = $credits != null ? $credits : $user->balance;
             $this->user = $user;
             $this->shop_id = $user->shop_id;
@@ -482,11 +482,11 @@ namespace VanguardLTE\Games\GatesofOlympusPM
         }
         public function GetBank($slotState = '')
         {
-            // if ($this->happyhouruser)
-            // {
-            //     $this->Bank = $this->happyhouruser->current_bank;
-            //     return $this->Bank / $this->CurrentDenom;
-            // }
+            if ($this->happyhouruser)
+            {
+                $this->Bank = $this->happyhouruser->current_bank;
+                return $this->Bank / $this->CurrentDenom;
+            }
             if( $this->isBonusStart || $slotState == 'bonus' || $slotState == 'freespin') 
             {
                 $slotState = 'bonus';
@@ -550,17 +550,17 @@ namespace VanguardLTE\Games\GatesofOlympusPM
             {
                 if($slotState == 'bonus'){
                     $diffMoney = $this->GetBank($slotState) + $sum;
-                    // if ($this->happyhouruser){
-                    //     $this->happyhouruser->increment('over_bank', abs($diffMoney));
-                    // }
-                    // else {
+                    if ($this->happyhouruser){
+                        $this->happyhouruser->increment('over_bank', abs($diffMoney));
+                    }
+                    else {
                         $normalbank = $game->get_gamebank('');
                         if ($normalbank + $diffMoney < 0)
                         {
                             $this->InternalError('Bank_   ' . $sum . '  CurrentBank_ ' . $this->GetBank($slotState) . ' CurrentState_ ' . $slotState);
                         }
                         $game->set_gamebank($diffMoney, 'inc', '');
-                    // }
+                    }
                     $sum = $sum - $diffMoney;
                 }else{
                     if ($sum < 0){
@@ -609,13 +609,13 @@ namespace VanguardLTE\Games\GatesofOlympusPM
             {
                 $this->toGameBanks = $sum;
             }
-            // if ($this->happyhouruser)
-            // {
-            //     $this->happyhouruser->increment('current_bank', $sum);
-            //     $this->happyhouruser->save();
-            // }
-            // else
-            // {
+            if ($this->happyhouruser)
+            {
+                $this->happyhouruser->increment('current_bank', $sum);
+                $this->happyhouruser->save();
+            }
+            else
+            {
                 if( $_obf_bonus_systemmoney > 0 ) 
                 {
                     $sum -= $_obf_bonus_systemmoney;
@@ -623,7 +623,7 @@ namespace VanguardLTE\Games\GatesofOlympusPM
                 }
                 $game->set_gamebank($sum, 'inc', $slotState);
                 $game->save();
-            // }
+             }
             return $game;
         }
         public function SetBalance($sum, $slotEvent = '')
@@ -807,16 +807,13 @@ namespace VanguardLTE\Games\GatesofOlympusPM
             $game->{'garant_win' . $_obf_granttype . $_obf_linecount} = $_obf_grantwin_count;
             $game->{'garant_bonus' . $_obf_granttype . $_obf_linecount} = $_obf_grantbonus_count;
             $game->save();
-            // if ($this->happyhouruser)
-            // {
-            //     $bonus_spin = rand(1, 10);
-            //     $spin_percent = 5;
-            //     if ($garantType == 'freespin')
-            //     {
-            //         $spin_percent = 3;
-            //     }
-            //     $spinWin = ($bonus_spin < $spin_percent) ? 1 : 0;
-            // }
+            if ($this->happyhouruser)
+            {
+                $bonus_spin = rand(1, 10);
+                $spin_percent = 5;
+
+                $spinWin = ($bonus_spin < $spin_percent) ? 1 : 0;
+            }
             if( $bonusWin == 1 && $this->slotBonus ) 
             {
                 $this->isBonusStart = true;
