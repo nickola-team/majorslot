ed7a7ad98d6c8e6fc1371e8b67c1228535c62d18
PHOO <phoo@gmail.com>
2021-08-07 00:05:13 +0800

    add 5Lions game

---
+++ app/Games/_5LionsPM/GameReel.php
@@ -0,0 +1,42 @@
+<?php 
+namespace VanguardLTE\Games\_5LionsPM
+{
+    class GameReel
+    {
+        public $reelsStrip = [
+            'reelStrip0_0' => [],
+            'reelStrip0_1' => [],
+            'reelStrip0_2' => [],
+            'reelStrip0_3' => [],
+            'reelStrip0_4' => [],
+
+            'reelStrip1_0' => [],
+            'reelStrip1_1' => [],
+            'reelStrip1_2' => [],
+            'reelStrip1_3' => [],
+            'reelStrip1_4' => [],
+        ];
+        
+        public function __construct()
+        {
+            $temp = file(base_path() . '/app/Games/_5LionsPM/reels.txt');
+            foreach( $temp as $str ) 
+            {
+                $str = explode('=', $str);
+                if( isset($this->reelsStrip[$str[0]]) ) 
+                {
+                    $data = explode(',', $str[1]);
+                    foreach( $data as $elem ) 
+                    {
+                        $elem = trim($elem);
+                        if( $elem != '' ) 
+                        {
+                            $this->reelsStrip[$str[0]][] = $elem;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
