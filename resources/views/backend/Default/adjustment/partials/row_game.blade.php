4cd89f209d0707ec8164b133a4abb118932dd266
PHOO <phoo@gmail.com>
2021-09-20 14:35:25 +0800

    change text color of game adj page

--- resources/views/backend/Default/adjustment/partials/row_game.blade.php
+++ resources/views/backend/Default/adjustment/partials/row_game.blade.php
@@ -11,13 +11,13 @@
 	@endif
 	</span>
 	</td>
-	<td ><span class="{{$total?'text-red':'text-black'}}"> {{ number_format($cat['totalbet'],0) }} </span></td>
-	<td ><span class="{{$total?'text-red':'text-black'}}"> {{ number_format($cat['totalwin'],0)}}</span></td>
-	<td ><span class="{{$total?'text-red':'text-black'}}"> {{ number_format($cat['totalbet'] - $cat['totalwin'],0)}}</span></td>
-	<td ><span class="{{$total?'text-red':'text-black'}}"> {{ number_format($cat['totalcount'])}}</span></td>
-	<td ><span class="{{$total?'text-red':'text-black'}}"> {{ number_format($cat['total_deal'],0)}}</span></td>
-	<td ><span class="{{$total?'text-red':'text-black'}}"> {{ number_format($cat['total_mileage'],0)}}</span></td>
-	<td ><span class="{{$total?'text-red':'text-black'}}"> {{ number_format($cat['total_deal'] - $cat['total_mileage'],0)}}</span></td>
+	<td ><span class="{{$total?'text-red':''}}"> {{ number_format($cat['totalbet'],0) }} </span></td>
+	<td ><span class="{{$total?'text-red':''}}"> {{ number_format($cat['totalwin'],0)}}</span></td>
+	<td ><span class="{{$total?'text-red':''}}"> {{ number_format($cat['totalbet'] - $cat['totalwin'],0)}}</span></td>
+	<td ><span class="{{$total?'text-red':''}}"> {{ number_format($cat['totalcount'])}}</span></td>
+	<td ><span class="{{$total?'text-red':''}}"> {{ number_format($cat['total_deal'],0)}}</span></td>
+	<td ><span class="{{$total?'text-red':''}}"> {{ number_format($cat['total_mileage'],0)}}</span></td>
+	<td ><span class="{{$total?'text-red':''}}"> {{ number_format($cat['total_deal'] - $cat['total_mileage'],0)}}</span></td>
 
 	@if ($loop->index > 0)
 	</tr>
