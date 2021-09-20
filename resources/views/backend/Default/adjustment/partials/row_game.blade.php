f8161bac27688ded47d09d243b3a6b99e52cf1a2
PHOO <phoo@gmail.com>
2021-09-20 14:41:18 +0800

    update text color of game adj

--- resources/views/backend/Default/adjustment/partials/row_game.blade.php
+++ resources/views/backend/Default/adjustment/partials/row_game.blade.php
@@ -3,7 +3,7 @@
 	<tr>
 	@endif
 	<td >
-	<span class="{{$total?'text-red':'text-black'}}">
+	<span class="{{$total?'text-red':''}}">
 	@if (!$total && auth()->user()->hasRole('admin') && Request::get('partner') == '' && Request::get('cat') == '')
 	<a href="{{route($admurl.'.adjustment_game', ['cat'=>$cat['category_id'], 'date' => $cat['date'], 'type'=>$cat['type']] ) }}"> {{$cat['title'] }} </a>
 	@else
