85deefe89dccedc028a52173ba415b9e3d76af5e
PHOO <phoo@gmail.com>
2021-10-03 21:37:33 +0800

    update zeus

--- app/Games/ZeusCQ9/Server.php
+++ app/Games/ZeusCQ9/Server.php
@@ -305,7 +305,7 @@ namespace VanguardLTE\Games\ZeusCQ9
                 {
                     break;
                 }
-                if( $scatterReelNumberCount >= 3 && ($winType != 'bonus' || $scatterReelNumberCount != $defaultScatterCount || ($totalWin - $scatterWin) == 0)) 
+                if( $scatterReelNumberCount >= 3 && ($winType != 'bonus' || $scatterReelNumberCount != $defaultScatterCount) 
                 {
                 }
                 else if( $totalWin <= $_winAvaliableMoney && $winType == 'bonus' ) 
