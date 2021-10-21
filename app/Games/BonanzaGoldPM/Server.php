7c5c6dace87c7ecd3a0ed7d0c8564e309500a017
PHOO <phoo@gmail.com>
2021-10-21 21:23:47 +0800

    bug fix tumble games

--- app/Games/BonanzaGoldPM/Server.php
+++ app/Games/BonanzaGoldPM/Server.php
@@ -164,7 +164,7 @@ namespace VanguardLTE\Games\BonanzaGoldPM
                         $response = '{"responseEvent":"error","responseType":"' . $slotEvent['slotEvent'] . '","serverResponse":"invalid bonus"}';
                         exit( $response );
                     }
-                    if( $slotEvent['slotEvent'] == 'doSpin' && $slotSettings->GetBalance() < ($lines * $betline) ) 
+                    if( $slotEvent['slotEvent'] == 'doSpin' && $slotSettings->GetBalance() < ($lines * $betline) && $slotSettings->GetGameData($slotSettings->slotId . 'TumbleState') == 0) 
                     {
                         $response = '{"responseEvent":"error","responseType":"' . $slotEvent['slotEvent'] . '","serverResponse":"invalid balance"}';
                         exit( $response );
