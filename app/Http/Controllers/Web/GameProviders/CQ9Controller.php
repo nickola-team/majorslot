3a4bf89e57be0a1579f8affa29e7f2634938dc6d
phoo <jphoo71719@gmail.com>
2021-11-10 22:55:03 +0800

    update constant of cq9

--- app/Http/Controllers/Web/GameProviders/CQ9Controller.php
+++ app/Http/Controllers/Web/GameProviders/CQ9Controller.php
@@ -8,7 +8,7 @@ namespace VanguardLTE\Http\Controllers\Web\GameProviders
         /*
         * UTILITY FUNCTION
         */
-        public static $LIVECODE = 'CA01';
+        const LIVECODE = 'CA01';
         function validRFC3339Date($date) {
             if (preg_match('/^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([\+|\-]([01][0-9]|2[0-3]):[0-5][0-9]))$/', $date)) {
                 return true;
