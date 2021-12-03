a6da914e4f44c6810c8f2ae2f98ea71b6a8ef59c
phoo <jphoo71719@gmail.com>
2021-12-03 22:12:45 +0800

    update deal convert api

--- app/Http/Controllers/Web/Frontend/ApiController.php
+++ app/Http/Controllers/Web/Frontend/ApiController.php
@@ -551,9 +551,8 @@ namespace VanguardLTE\Http\Controllers\Web\Frontend
             }
 
             $summ = str_replace(',','',$request->summ);
-           
+            $shop = \VanguardLTE\Shop::lockForUpdate()->where('id',$user->shop_id)->first();           
             if($user->hasRole('manager')){
-                $shop = \VanguardLTE\Shop::lockForUpdate()->where('id',$user->shop_id)->first();
                 if (!$shop)
                 {
                     return response()->json([
@@ -585,7 +584,89 @@ namespace VanguardLTE\Http\Controllers\Web\Frontend
                 $summ = $real_deal_balance;
             }
 
-            \VanguardLTE\Jobs\OutDeal::dispatch(['user_id' => $user->id, 'sum' => abs($summ)])->onQueue('deal');
+            // \VanguardLTE\Jobs\OutDeal::dispatch(['user_id' => $user->id, 'sum' => abs($summ)])->onQueue('deal');
+
+            if ($summ > 0) {
+                //out balance from master
+                $master = $user->referral;
+                while ($master!=null && !$master->isInoutPartner())
+                {
+                    $master = $master->referral;
+                }
+                if ($master == null)
+                {
+                    return response()->json([
+                        'error' => true, 
+                        'msg' => '총본사를 찾을수 없습니다.',
+                        'code' => '000'
+                    ], 200);
+                }
+                
+                if ($master->balance < $summ)
+                {
+                    return response()->json([
+                        'error' => true, 
+                        'msg' => '총본사보유금이 부족합니다',
+                        'code' => '000'
+                    ], 200);
+                }
+                $master->update(
+                    ['balance' => $master->balance - $summ ]
+                );
+                $master = $master->fresh();
+
+                if ($user->hasRole('manager'))
+                {
+                    // $shop = \VanguardLTE\Shop::lockForUpdate()->where('id',$user->shop_id)->first();
+                    // $real_deal_balance = $shop->deal_balance - $shop->mileage;
+                    // if ($real_deal_balance < $summ)
+                    // {
+                    //     return -4;
+                    // }
+                    $old = $shop->balance;
+                    $shop->balance = $shop->balance + $summ;
+                    $shop->deal_balance = $real_deal_balance - $summ;
+                    $shop->mileage = 0;
+                    $shop->save();
+                    $shop = $shop->fresh();
+                    \VanguardLTE\ShopStat::create([
+                        'user_id' => $master->id,
+                        'type' => 'deal_out',
+                        'sum' => $summ,
+                        'old' => $old,
+                        'new' => $shop->balance,
+                        'balance' => $master->balance,
+                        'shop_id' => $shop->id,
+                        'date_time' => \Carbon\Carbon::now()
+                    ]);
+                }
+                else
+                {
+                    // $real_deal_balance = $user->deal_balance - $user->mileage;
+                    // if ($real_deal_balance < $summ)
+                    // {
+                    //     return -4;
+                    // }
+                    $old = $user->balance;
+                    $user->balance = $user->balance + $summ;
+                    $user->deal_balance = $real_deal_balance - $summ;
+                    $user->mileage = 0;
+                    $user->save();
+                    $user = $user->fresh();
+        
+                    \VanguardLTE\Transaction::create([
+                        'user_id' => $user->id,
+                        'payeer_id' => $master->id,
+                        'system' => $user->username,
+                        'type' => 'deal_out',
+                        'summ' => $summ,
+                        'old' => $old,
+                        'new' => $user->balance,
+                        'balance' => $master->balance,
+                        'shop_id' => 0
+                    ]);
+                }
+            }
             
             return response()->json(['error' => false]);
         }
