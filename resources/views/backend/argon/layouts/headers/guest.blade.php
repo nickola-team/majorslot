0ce203ea9e9816289af768c73af12dae5b718bc7
phoo <jphoo71719@gmail.com>
2022-01-25 23:16:33 +0800

    update login page

--- resources/views/backend/argon/layouts/headers/guest.blade.php
+++ resources/views/backend/argon/layouts/headers/guest.blade.php
@@ -11,7 +11,7 @@
     <div class="separator separator-bottom separator-skew zindex-100">
         <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <!-- <polygon class="fill-light" points="2560 0 2560 100 0 100"></polygon> -->
-            <ellipse class="fill-light" cx="1280" cy="100" rx="1280" ry="100"></ellipse>
+            <ellipse class="fill-light" cx="1280" cy="110" rx="1300" ry="100"></ellipse>
         </svg>
     </div>
 </div>
