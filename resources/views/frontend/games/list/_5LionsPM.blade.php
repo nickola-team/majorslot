3adf0b7637bed3785cb1c2cb35f21778e985ebf7
phoo <jphoo71719@gmail.com>
2022-03-16 10:08:31 +0800

    change pm directory name

--- resources/views/frontend/games/list/_5LionsPM.blade.php
+++ resources/views/frontend/games/list/_5LionsPM.blade.php
@@ -35,7 +35,7 @@ document.location.href='../../';
 
 
 
-<iframe id='game' style="margin:0px;border:0px;width:100%;height:100vh;" src='/games/{{ $game->name }}/openGame.do?lang=en&cur=@if( auth()->user()->present()->shop ){{ auth()->user()->present()->shop->currency }}@endif&extGame=1&gameSymbol=vs243lions&websiteUrl=&lobbyURL=' allowfullscreen>
+<iframe id='game' style="margin:0px;border:0px;width:100%;height:100vh;" src='/games/_{{ $game->name }}/openGame.do?lang=en&cur=@if( auth()->user()->present()->shop ){{ auth()->user()->present()->shop->currency }}@endif&extGame=1&gameSymbol=vs243lions&websiteUrl=&lobbyURL=' allowfullscreen>
 
 
 </iframe>
