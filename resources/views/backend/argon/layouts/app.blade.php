faeaae1522973161ee6223e0abc543b04ac56100
phoo <jphoo71719@gmail.com>
2022-03-21 21:47:43 +0800

    change button name

--- resources/views/backend/argon/layouts/app.blade.php
+++ resources/views/backend/argon/layouts/app.blade.php
@@ -54,10 +54,10 @@
                 <div id="footer">
                     <div class="row">
                         <div class="col-lg-6 m-auto">
-                        <button type="button" class="btn btn-primary col-12" id="doSubmit" onclick="closeNotification('notification{{$notice->id}}', false);">하루동안 보지 않기</button>
+                        <button type="button" class="btn btn-primary col-12" id="noticeDaily" onclick="closeNotification('notification{{$notice->id}}', false);">하루동안 보지 않기</button>
                         </div>
                         <div class="col-lg-6 m-auto">
-                        <button type="button" class="btn btn-default col-12" id="doSubmit" onclick="closeNotification('notification{{$notice->id}}', true);">닫기</button>
+                        <button type="button" class="btn btn-default col-12" id="noticeOnce" onclick="closeNotification('notification{{$notice->id}}', true);">닫기</button>
                         </div>
                     </div>
                 </div>
