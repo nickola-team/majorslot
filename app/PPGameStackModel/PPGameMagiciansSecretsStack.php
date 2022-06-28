2412dbc070661b33f736957687485414c5300d3d
Akapov <Akapov@example.com>
2022-06-28 09:30:02 +0800

    added MagiciansSecrets game

---
+++ app/PPGameStackModel/PPGameMagiciansSecretsStack.php
@@ -0,0 +1,23 @@
+<?php 
+namespace VanguardLTE\PPGameStackModel;
+{
+    class PPGameMagiciansSecretsStack extends \Illuminate\Database\Eloquent\Model
+    {
+        protected $table = 'ppgame_magicianssecrets_stack';
+        protected $fillable = [
+            'id', 
+            'game_id', 
+            'odd', 
+            'spin_stack', 
+            'spin_type', 
+            'pur_level', 
+            'fs_max'
+        ];
+        
+        public static function boot()
+        {
+            parent::boot();
+        }
+    }
+
+}
