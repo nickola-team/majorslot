0065578dfcd892a27166d5a7ee25aba4df472e5c
Akapov <Akapov@example.com>
2022-07-07 22:50:53 +0800

    update ReleaseTheKraken

--- app/Games/ReleasetheKrakenPM/Server.php
+++ app/Games/ReleasetheKrakenPM/Server.php
@@ -234,7 +234,7 @@ namespace VanguardLTE\Games\ReleasetheKrakenPM
                         $response = '{"responseEvent":"error","responseType":"' . $slotEvent['slotEvent'] . '","serverResponse":"invalid bet state"}';
                         exit( $response );
                     }
-                    if( $slotEvent['slotEvent'] == 'doSpin' && $slotSettings->GetBalance() < ($lines * $betline) ) 
+                    if( $slotEvent['slotEvent'] == 'doSpin' && $slotSettings->GetGameData($slotSettings->slotId . 'RespinType') == '' && $slotSettings->GetBalance() < ($lines * $betline) ) 
                     {
                         $response = '{"responseEvent":"error","responseType":"' . $slotEvent['slotEvent'] . '","serverResponse":"invalid balance"}';
                         exit( $response );
