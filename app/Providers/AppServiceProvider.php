0cab45f203cf0a25b4258c478450341f130f83b3
major <major@major.com>
2022-08-23 15:24:31 +0900

    bug fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Collection;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Pagination\LengthAwarePaginator;
-
+use Illuminate\Support\Facades\Request;
 use VanguardLTE\Http\Middleware\ThrottleSimultaneousRequests;
 class AppServiceProvider extends ServiceProvider
 {
