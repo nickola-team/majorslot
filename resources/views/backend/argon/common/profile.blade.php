3dcc77388737a648d6d1770fda81f5247d0e55e1
PHOO <phoo@mail.com>
2022-11-30 15:22:29 +0900

    update profile page

--- resources/views/backend/argon/common/profile.blade.php
+++ resources/views/backend/argon/common/profile.blade.php
@@ -194,10 +194,10 @@
                                             <th>파워볼조합롤링%</th>
                                         </tr>
                                         <tr>
-                                            <td style="padding:3px;"><input type="text" name="deal_percent" id="deal_percent" class="form-control" value="{{$user->deal_percent}}"></td>
-                                            <td style="padding:3px;"><input type="text" name="table_deal_percent" id="table_deal_percent" class="form-control" value="{{$user->table_deal_percent}}"></td>
-                                            <td style="padding:3px;"><input type="text" name="pball_single_percent" id="pball_single_percent" class="form-control" value="{{$user->pball_single_percent}}"></td>
-                                            <td style="padding:3px;"><input type="text" name="pball_comb_percent" id="pball_comb_percent" class="form-control" value="{{$user->pball_comb_percent}}"></td>
+                                            <td style="padding:3px;"><input type="text" name="deal_percent" id="deal_percent" class="form-control" value="{{$user->deal_percent}}" {{$user->id==auth()->user()->id?'disabled':''}}></td>
+                                            <td style="padding:3px;"><input type="text" name="table_deal_percent" id="table_deal_percent" class="form-control" value="{{$user->table_deal_percent}}" {{$user->id==auth()->user()->id?'disabled':''}}></td>
+                                            <td style="padding:3px;"><input type="text" name="pball_single_percent" id="pball_single_percent" class="form-control" value="{{$user->pball_single_percent}}" {{$user->id==auth()->user()->id?'disabled':''}}></td>
+                                            <td style="padding:3px;"><input type="text" name="pball_comb_percent" id="pball_comb_percent" class="form-control" value="{{$user->pball_comb_percent}}" {{$user->id==auth()->user()->id?'disabled':''}}></td>
                                         </tr>
                                     </table>
                                 </div>
