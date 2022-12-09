ace7543a42e68a1d022a53c152945c8629a52f75
PHOO <phoo@mail.com>
2022-12-09 12:10:02 +0900

    update daily report

--- resources/views/backend/argon/report/partials/row_dailydw.blade.php
+++ resources/views/backend/argon/report/partials/row_dailydw.blade.php
@@ -8,7 +8,10 @@
 <td>{{ number_format($adjustment->moneyin,0) }}</td>
 <td>{{ number_format($adjustment->moneyout,0) }}</td>
 <td>{{ number_format($adjustment->dealout,0) }}</td>
+<td>{{ number_format($adjustment->totalbet,0)}}</td>
+<td>{{ number_format($adjustment->totalwin,0)}}</td>
 <td>{{ number_format($adjustment->totalbet-$adjustment->totalwin,0) }}</td>
+<td>{{ number_format($adjustment->total_deal-$adjustment->total_mileage,0)}}</td>
 <td>{{ number_format($adjustment->balance+$adjustment->childsum,0)}}</td>
 @if (auth()->user()->hasRole('admin'))
 <td>
