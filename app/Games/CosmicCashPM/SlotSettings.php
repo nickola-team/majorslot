e6014b78a0be6af4a5b6e274f8a5a0dda41af798
Akapov <Akapov@example.com>
2022-12-23 14:20:24 +0800

    update cosmiccash

--- app/Games/CosmicCashPM/SlotSettings.php
+++ app/Games/CosmicCashPM/SlotSettings.php
@@ -268,7 +268,7 @@ namespace VanguardLTE\Games\CosmicCashPM
                 $this->Bank = $this->happyhouruser->current_bank;
                 return $this->Bank / $this->CurrentDenom;
             }
-        if( $this->isBonusStart || $slotState == 'bonus' || $slotState == 'doBonus' || $slotState == 'freespin' || $slotState == 'respin' ) 
+        if( $this->isBonusStart || $slotState == 'bonus' || $slotState == 'doBonus' || $slotState == 'freespin' || $slotState == 'respin'  || $slotState == 'doRespin') 
 
 
             {
@@ -310,7 +310,7 @@ namespace VanguardLTE\Games\CosmicCashPM
         }
         public function SetBank($slotState = '', $sum, $slotEvent = '', $isFreeSpin = false)
         {
-        if( $this->isBonusStart || $slotState == 'bonus' || $slotState == 'doBonus' || $slotState == 'freespin' || $slotState == 'respin' ) 
+        if( $this->isBonusStart || $slotState == 'bonus' || $slotState == 'doBonus' || $slotState == 'freespin' || $slotState == 'respin' || $slotState == 'doRespin' ) 
 
 
             {
