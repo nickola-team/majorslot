0138a25562c076b432511efdfc7a7c2bce109c5c
PHOO <phoo@mail.com>
2023-01-11 15:19:42 +0900

    update tp game round

--- app/Http/Controllers/Web/GameProviders/TPController.php
+++ app/Http/Controllers/Web/GameProviders/TPController.php
@@ -488,7 +488,7 @@ namespace VanguardLTE\Http\Controllers\Web\GameProviders
                         }
                     }
 
-                    $category = \VanguardLTE\Category::where(['provider' => self::TP_PROVIDER, 'shop_id' => 0, 'href' => self::TP_IDENTITY_GAME[$round['ThirdParty']]])->first();
+                    $category = \VanguardLTE\Category::where(['provider' => self::TP_PROVIDER,'href' => self::TP_IDENTITY_GAME[$round['ThirdParty']]])->first();
 
                     $balance = $round['Balance'];
                     $time = $round['Date'];
