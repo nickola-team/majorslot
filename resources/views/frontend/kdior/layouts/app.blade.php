a39030a86654f28ec6d92c526a2329929575944f
PHOO <phoo@mail.com>
2023-02-02 22:29:20 +0900

    update kdior

--- resources/views/frontend/kdior/layouts/app.blade.php
+++ resources/views/frontend/kdior/layouts/app.blade.php
@@ -1,3 +1,8 @@
+
+<?php
+	$detect = new \Detection\MobileDetect();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,10 +10,16 @@
 <title>@yield('page-title')</title>
 <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;">
 <link rel="shortcut icon" href="/frontend/kdior/favicon.ico" type="image/x-icon">
+<link href="/frontend/kdior/css/font.css?v=202301301150" rel="stylesheet" type="text/css">
+@if( $detect->isMobile() || $detect->isTablet() ) 
+<link href="/frontend/kdior/css/m/common.css?v=202301301150" rel="stylesheet" type="text/css">
+<link href="/frontend/kdior/css/m/basic.css?v=202301301150" rel="stylesheet" type="text/css">
+<link href="/frontend/kdior/css/m/layout.css?v=202301301150" rel="stylesheet" type="text/css">
+@else
 <link href="/frontend/kdior/css/common.css?v=202301301150" rel="stylesheet" type="text/css">
 <link href="/frontend/kdior/css/basic.css?v=202301301150" rel="stylesheet" type="text/css">
-<link href="/frontend/kdior/css/font.css?v=202301301150" rel="stylesheet" type="text/css">
 <link href="/frontend/kdior/css/layout.css?v=202301301150" rel="stylesheet" type="text/css">
+@endif
 <link rel="stylesheet" href="/frontend/kdior/css/animations.css?v=202301301150"><!-- CSS animations1 -->
 <script language="javascript" src="/frontend/kdior/js/showid.js" type="text/javascript"></script>
 <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script><!-- sk_기본 -->
@@ -17,34 +28,41 @@
 <script src="/frontend/kdior/js/func.js"></script>
 </head>
 
-
-<body class="overflow-x:hidden;">
+<body>
 	<!-- ★메인★ -->
 	<div id="wrap">
-		@include('frontend.kdior.layouts.partials.header')
-        @include('frontend.kdior.layouts.partials.banner')
-		
+		@if( $detect->isMobile() || $detect->isTablet() ) 
+			@include('frontend.kdior.layouts.partials.m.header')
+			@include('frontend.kdior.layouts.partials.m.content')
+		@else
+			@include('frontend.kdior.layouts.partials.header')
+			@include('frontend.kdior.layouts.partials.banner')
+			
 
-		<div class="main_contents_wrap">
-			<div class="main_contents_box">
-				@include('frontend.kdior.layouts.partials.category')
-				@include('frontend.kdior.layouts.partials.board')
+			<div class="main_contents_wrap">
+				<div class="main_contents_box">
+					@include('frontend.kdior.layouts.partials.category')
+					@include('frontend.kdior.layouts.partials.board')
+				</div>
 			</div>
-		</div>
+		@endif
+
 		@include('frontend.kdior.layouts.partials.footer')
 		
 	</div><!-- wrap -->
-
-	@yield('content')
-	@auth()
-		@include('frontend.kdior.modals.common')
+	@if( $detect->isMobile() || $detect->isTablet() ) 
 	@else
-		@include('frontend.kdior.modals.login')
-		@include('frontend.kdior.modals.join')
+		@yield('content')
+		@auth()
+			@include('frontend.kdior.modals.common')
+		@else
+			@include('frontend.kdior.modals.login')
+			@include('frontend.kdior.modals.join')
+		@endif
+		@foreach ($noticelist as $ntc)
+		@include('frontend.kdior.modals.notice', ['notice' => $ntc])
+		@endforeach
 	@endif
-	@foreach ($noticelist as $ntc)
-      @include('frontend.kdior.modals.notice', ['notice' => $ntc])
-    @endforeach
 </body>
 </html>
 
