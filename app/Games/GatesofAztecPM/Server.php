a66977c4afa57b1fa9b394f6e4784e54deda94b0
wangpingping5 <dev.wangping@gmail.com>
2023-02-07 21:09:57 +0800

    update 5 games like StarlightChristmas

--- app/Games/GatesofAztecPM/Server.php
+++ app/Games/GatesofAztecPM/Server.php
@@ -256,7 +256,7 @@ namespace VanguardLTE\Games\GatesofAztecPM
                         $response = '{"responseEvent":"error","responseType":"' . $slotEvent['slotEvent'] . '","serverResponse":"invalid bet state"}';
                         exit( $response );
                     }
-                    if( $slotEvent['slotEvent'] == 'doSpin' && $slotSettings->GetBalance() < ($lines * $betline)  && $slotSettings->GetGameData($slotSettings->slotId . 'TumbleState') == 0) 
+                    if( $slotEvent['slotEvent'] == 'doSpin' && $slotSettings->GetBalance() < ($lines * $betline)  && $slotSettings->GetGameData($slotSettings->slotId . 'CurrentRespin') < 0) 
                     {
                         $response = '{"responseEvent":"error","responseType":"' . $slotEvent['slotEvent'] . '","serverResponse":"invalid balance"}';
                         exit( $response );
