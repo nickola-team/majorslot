e93f1263a5842d43a22abeb101ebbdd2fc8f0e62
wangpingping5 <dev.wangping@gmail.com>
2023-02-16 22:01:41 +0800

    update doublechance

--- app/Games/AztecGemsDeluxePM/SlotSettings.php
+++ app/Games/AztecGemsDeluxePM/SlotSettings.php
@@ -578,7 +578,7 @@ namespace VanguardLTE\Games\AztecGemsDeluxePM
             $winline_count = $game->{'winline' . $granttype . $linecount};
             $grantwin_count++;
             if($isdoublechance == 1){
-                $grantbonus_count+=2;
+                $grantbonus_count++;
             }else{
                 $grantbonus_count++;
             }
--- app/Games/FortuneofGizaPM/SlotSettings.php
+++ app/Games/FortuneofGizaPM/SlotSettings.php
@@ -580,7 +580,7 @@ namespace VanguardLTE\Games\FortuneofGizaPM
             $winline_count = $game->{'winline' . $granttype . $linecount};
             $grantwin_count++;
             if($isdoublechance == 1){
-                $grantbonus_count+=2;
+                $grantbonus_count++;
             }else{
                 $grantbonus_count++;
             }
