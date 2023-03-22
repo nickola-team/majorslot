26e50759569eca6967c1387a8945a257b8660686
PHOO <phoo@mail.com>
2023-03-22 15:22:37 +0900

    update game sync module

--- game-sync.js
+++ game-sync.js
@@ -1,9 +1,9 @@
 const { exec } = require("child_process");
 
 
-let betwinSync = 60000;
-let exitSync = 60000;
-let launchSync = 2000;
+let betwinSync = 300000;
+let exitSync = 300000;
+let launchSync = 3000;
 
 console.log('======= Starting Game Launch  thread =============');
 console.log(' Bet/Win Sync = %d seconds', betwinSync / 1000);
