128bef84e017439558ff85c2a8ebfab620114a90
wangpingping5 <dev.wangping@gmail.com>
2023-04-18 16:51:44 +0800

    revert test

--- app/Games/moonsistersbng/Server.php
+++ app/Games/moonsistersbng/Server.php
@@ -24,6 +24,7 @@ namespace VanguardLTE\Games\moonsistersbng
                 $response = '{"responseEvent":"error","responseType":"error","serverResponse":"Error LicenseDK"}';
                 exit( $response );
             }*/
+            $str='';
             $response = '';
             \DB::beginTransaction();
             // $userId = \Auth::id();// changed by game developer
