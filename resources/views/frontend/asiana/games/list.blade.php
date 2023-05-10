bc246177739bad504046d491624ff88813e142ba
major <major@major.com>
2023-05-10 11:19:13 +0900

    add 8 design

---
+++ resources/views/frontend/asiana/games/list.blade.php
@@ -0,0 +1,6 @@
+@extends('frontend.asiana.layouts.app')
+@section('page-title', $title)
+
+@section('content')
+@include('frontend.kdior.games.index')
+@stop
---
+++ resources/views/frontend/world01/games/list.blade.php
@@ -0,0 +1,6 @@
+@extends('frontend.world01.layouts.app')
+@section('page-title', $title)
+
+@section('content')
+@include('frontend.kdior.games.index')
+@stop
