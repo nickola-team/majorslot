edb32e15997dadd1165d40e7524a200a509a4788
major <major@major.com>
2023-05-18 11:56:34 +0900

    hide daily betwin

--- resources/views/backend/argon/layouts/navbars/sidebar.blade.php
+++ resources/views/backend/argon/layouts/navbars/sidebar.blade.php
@@ -331,12 +331,12 @@
                                     일별정산
                                 </a>
                             </li>
-                            <li class="nav-item">
+                            <!-- <li class="nav-item">
                                 <a class="nav-link text-white" href="{{argon_route('argon.report.daily')}}">
                                 <i class="far fa-circle text-white sub-i"></i>
                                     일별벳윈
                                 </a>
-                            </li>
+                            </li> -->
                             <!-- <li class="nav-item">
                                 <a class="nav-link text-white" href="{{argon_route('argon.report.monthly')}}">
                                 <i class="far fa-circle text-white sub-i"></i>
