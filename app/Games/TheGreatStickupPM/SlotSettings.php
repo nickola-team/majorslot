ac4ca718d0fd51637250afc4011d57056c866ada
wangpingping5 <dev.wangping@gmail.com>
2023-05-22 10:46:18 +0800

    update winbonus3 function

--- app/Games/TheGreatStickupPM/SlotSettings.php
+++ app/Games/TheGreatStickupPM/SlotSettings.php
@@ -743,11 +743,11 @@ namespace VanguardLTE\Games\TheGreatStickupPM
                     }else{
                         $index = mt_rand(0, 83000);
                     }
-                    if($bet > $this->game->limitBonusMoney && $limitOdd > 10 && $this->game->garant_bonus3 >= $this->game->winbonus3){
+                    if($bet > $this->game->special_limitmoney && $limitOdd > 10 && $this->game->grant_special_winbonus >= $this->game->special_winbonus){
                         $stacks = $stacks->where('odd', '<=', $limitOdd)->orderby('odd', 'desc')->take(100)->get();
-                        $this->game->garant_bonus3 = 0;
-                        $win = explode(',', $this->game->game_win->winbonus3);
-                        $this->game->winbonus3 = $win[rand(0, count($win) - 1)];
+                        $this->game->grant_special_winbonus = 0;
+                        $win = explode(',', $this->game->game_win->special_winbonus);
+                        $this->game->special_winbonus = $win[rand(0, count($win) - 1)];
                         $this->game->save();
                     }else{
                         if($winType == 'bonus'){
