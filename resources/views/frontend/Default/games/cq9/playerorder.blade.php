22a5e56ba8f83c753a0c89dd81ab0160c22f6f25
wangpingping5 <dev.wangping@gmail.com>
2023-06-27 22:42:51 +0800

    update playerorder.blade.php

--- resources/views/frontend/Default/games/cq9/playerorder.blade.php
+++ resources/views/frontend/Default/games/cq9/playerorder.blade.php
@@ -19,12 +19,12 @@
         font-size: 16px;
         background: #2A2A2A !important;
         }</style>
-        <link href=/cq9history/static/app.23bd565ad12ecf88b6cccca31309a36a.css rel=stylesheet>
+        <link href=/cq9history/static/app.f08d7eb39241fe4dd3b394c1e5737d58.css rel=stylesheet>
     </head>
     <body class=Theme1>
         <div id=Container></div>
-        <script type=text/javascript src=/cq9history/static/js/manifest.07ac520d239b7ee4944e.js></script>
-        <script type=text/javascript src=/cq9history/static/js/vendor.4331b9933ea51f54e546.js></script>
-        <script type=text/javascript src=/cq9history/static/js/app.eb44a30b4bbaa7b1502b.js></script>
+        <script type=text/javascript src=/cq9history/static/js/manifest.adb7fb9d3676bbf26ee6.js></script>
+        <script type=text/javascript src=/cq9history/static/js/vendor.b28c9b84b66bb0b6dc4f.js></script>
+        <script type=text/javascript src=/cq9history/static/js/app.007b6f1992aa6bafc269.js></script>
     </body>
 </html>
