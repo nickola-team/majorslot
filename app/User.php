b82c4c8aad22c9e576f1508188cd2a0ba074c9d7
wangpingping5 <dev.wangping@gmail.com>
2023-08-09 16:02:27 +0800

    added sky frontpage and csv function

--- app/User.php
+++ app/User.php
@@ -1443,20 +1443,31 @@ namespace VanguardLTE
             }
             
         }
-        public static function exportcsv($user_id){
-            $user = \VanguardLTE\User::where('id', $user_id)->first();         
-            $strlog =  $user->id . "###".$user->username."###".$user->parent_id."###".$user->role_id."###".$user->phone."###".$user->bank_name."###".$user->recommender."###".$user->account_no."\n";
-            if( file_exists(storage_path('logs/') . '') ) 
-            {
-                $strinternallog = file_get_contents(storage_path('logs/') .'UserList.log');
+        public $export_csvUserList = [];
+        public function getCSVUserList($user_id){
+            $user = \VanguardLTE\User::where('id', $user_id)->first();   
+            $parent_name = '';
+            if($user->parent_id > 0){
+                $parent_name = $user->referral->username;
             }
-            file_put_contents(storage_path('logs/') . 'UserList.log', $strinternallog . $strlog);
+            $this->export_csvUserList[] = [
+                'id' => $user->id,
+                'username' => $user->username,
+                'parent_id' => $parent_name,
+                'role_id' => $user->role->description,
+                'phone' => $user->phone,
+                'bank_name' => $user->bank_name,
+                'recommender' => $user->recommender,
+                'account_no' => $user->account_no,
+                'balance' => $user->balance
+            ];
             if($user->role_id > 1){
                 $childUsers = User::where('parent_id', $user->id)->get();
                 for($i = 0; $i < count($childUsers); $i++){
-                    User::exportcsv($childUsers[$i]->id);
+                    $this->getCSVUserList($childUsers[$i]->id);
                 }
             }
+            return $this->export_csvUserList;
         }
     }
 
