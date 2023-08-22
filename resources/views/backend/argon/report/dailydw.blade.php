77ee7212648a0feaaef87f8bb887417f2c569b7d
wangpingping5 <dev.wangping@gmail.com>
2023-08-22 18:50:45 +0800

    update dailydw

--- resources/views/backend/argon/report/dailydw.blade.php
+++ resources/views/backend/argon/report/dailydw.blade.php
@@ -278,7 +278,12 @@
                                             <li>
                                                 <span class='text-red'>당첨 : {{number_format($bt['totaldealwin'])}}</span>
                                             </li>
-                                            
+                                            <li>
+                                            롤링금 : {{number_format($bt['total_deal']>0?$bt['total_deal']:$bt['total_mileage'])}} 
+                                            </li>
+                                            <li>
+                                            정산금 : {{number_format($bt['totaldealbet']-$bt['totaldealwin'] -($bt['total_deal']>0?$bt['total_deal']:$bt['total_mileage']))}}
+                                            </li>
                                             </ul>
                                         </div>
                                         
