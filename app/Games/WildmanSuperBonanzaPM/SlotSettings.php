c9197a66cccced9d4ecacbc9be68ac47b436743f
wangpingping5 <dev.wangping@gmail.com>
2023-09-22 22:19:51 +0800

    update happyuser for 189 files

--- app/Games/WildmanSuperBonanzaPM/SlotSettings.php
+++ app/Games/WildmanSuperBonanzaPM/SlotSettings.php
@@ -745,6 +745,7 @@ namespace VanguardLTE\Games\WildmanSuperBonanzaPM
                 $limitOdd = $this->GetBank('') / $bet;
                 if($limitOdd > 10){
                     $winType = 'bonus';
+                    $spintype = 1;
                 }else if($limitOdd > 1){
                     $winType = 'win';
                 }else{
