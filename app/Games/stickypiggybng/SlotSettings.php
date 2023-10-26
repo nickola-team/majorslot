62e0ece9342753063ca226cfa3a3de7e9ae4eb91
wangpingping5 <dev.wangping@gmail.com>
2023-10-26 11:07:03 +0800

    update booongo for call

--- app/Games/stickypiggybng/SlotSettings.php
+++ app/Games/stickypiggybng/SlotSettings.php
@@ -298,7 +298,9 @@ namespace VanguardLTE\Games\stickypiggybng
             if($isFreeSpin == true){
                 if ($this->happyhouruser)
                 {
+                    if($sum > 0){
                     $this->happyhouruser->increment('current_bank', $sum);
+                }
                     $this->happyhouruser->save();
                     return $game;
                 }
@@ -316,7 +318,7 @@ namespace VanguardLTE\Games\stickypiggybng
                 if($slotState == 'bonus'){
                     $diffMoney = $this->GetBank($slotState) + $sum;
                     if ($this->happyhouruser){
-                        $this->happyhouruser->increment('over_bank', abs($diffMoney));
+                        //$this->happyhouruser->increment('over_bank', abs($diffMoney));
                     }
                     else {
                         $normalbank = $game->get_gamebank('');
@@ -376,7 +378,9 @@ namespace VanguardLTE\Games\stickypiggybng
             }
             if ($this->happyhouruser)
             {
-                $this->happyhouruser->increment('current_bank', $sum);
+                if($sum > 0){
+                    $this->happyhouruser->increment('current_bank', $sum);
+                }
                 $this->happyhouruser->save();
             }
             else
@@ -768,8 +772,12 @@ namespace VanguardLTE\Games\stickypiggybng
                     break;
                 }
             }
-            $stack = $stacks[rand(0, count($stacks) - 1)]->spin_stack;
-            return json_decode($stack, true);
+            $stack = $stacks[rand(0, count($stacks) - 1)];
+            if($this->happyhouruser){
+                $sum = -1 * $stack->odd * $bet;
+                $this->happyhouruser->increment('current_bank', $sum);
+            }
+	        return json_decode($stack->spin_stack, true);
         }
     }
 }
