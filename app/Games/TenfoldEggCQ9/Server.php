53fd82760b22cf0a277e4d202763253f944807ef
wangpingping5 <dev.wangping@gmail.com>
2023-11-21 17:34:26 +0800

    update tenfoldegg

--- app/Games/TenfoldEggCQ9/Server.php
+++ app/Games/TenfoldEggCQ9/Server.php
@@ -272,8 +272,8 @@ namespace VanguardLTE\Games\TenfoldEggCQ9
             $_winAvaliableMoney = $_spinSettings[1];
             $purValue = -1;
             $tempPurValue = -1;
-            $winType = 'bonus';
-            $_winAvaliableMoney = $slotSettings->GetBank($slotEvent);
+            // $winType = 'bonus';
+            // $_winAvaliableMoney = $slotSettings->GetBank($slotEvent);
             
             if($slotEvent == 'bonus' || $slotEvent == 'respin'){
                 $tumbAndFreeStacks = $slotSettings->GetGameData($slotSettings->slotId . 'TumbAndFreeStacks');
@@ -453,7 +453,7 @@ namespace VanguardLTE\Games\TenfoldEggCQ9
                             $stack['ExtendFeatureByGame2'][7]['Value'] = strval($tempStr);
                             $stack['ExtendFeatureByGame2'][8]['Value'] = strval($tempStr);
     
-                            $$tempSpinArr = [1,1,0];
+                            $tempSpinArr = [1,1,0];
                         }else if($purValue == 2){ //0 1 1
                             $purValue = 5;
                             $stack['ExtendFeatureByGame2'][5]['Value'] = '1';
