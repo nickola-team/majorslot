2edd15af5d8830b8d18f79da168e1df1ac79d8ee
PetarGlomuz <petarmaster1997@gmail.com>
2023-12-18 18:50:47 +0800

    CQ Moneytree add

--- app/Games/MoneyTreeCQ9/SlotSettings.php
+++ app/Games/MoneyTreeCQ9/SlotSettings.php
@@ -450,11 +450,11 @@ namespace VanguardLTE\Games\MoneyTreeCQ9
             else
             {
             //------- *** -------//
-                /*if( $_obf_bonus_systemmoney > 0 )         ///free game 없는 경우 이 부분 disable
+                if( $_obf_bonus_systemmoney > 0 )         ///free game 없는 경우 이 부분 disable
                 {
                     $sum -= $_obf_bonus_systemmoney;
                     $game->set_gamebank($_obf_bonus_systemmoney, 'inc', 'bonus');
-                }*/
+                }
                 $game->set_gamebank($sum, 'inc', $slotState);
                 $game->save();
             }
