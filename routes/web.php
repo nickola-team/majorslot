336ed87ac5d6d848364c70dd2c0378027a59bdd4
wangpingping5 <dev.wangping@gmail.com>
2024-03-01 09:55:54 +0900

    added 4 pp games and update ppfreestacklog

--- routes/web.php
+++ routes/web.php
@@ -1473,8 +1473,11 @@ Route::group(['prefix' => 'gs2c',], function () {
     Route::get('/minilobby/start', 'GameProviders\PPController@minilobby_start');
     Route::get('lastGameHistory.do', 'GameProviders\PPController@ppHistory');
     Route::get('api/history/v2/settings/general', 'GameProviders\PPController@general');
+    Route::post('api/history/v2/settings/general', 'GameProviders\PPController@general');
     Route::get('api/history/v2/play-session/last-items', 'GameProviders\PPController@last_items');
+    Route::post('api/history/v2/play-session/last-items', 'GameProviders\PPController@last_items');
     Route::get('api/history/v2/action/children', 'GameProviders\PPController@children');    
+    Route::post('api/history/v2/action/children', 'GameProviders\PPController@children');    
     Route::get('session/play-verify/{gamecode}', 'GameProviders\PPController@verify');    
     Route::get('session/verify/{gamecode}', 'GameProviders\KTENController@ppverify');  
 });
