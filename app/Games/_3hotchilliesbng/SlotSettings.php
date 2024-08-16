3dc47dcc76ccd25e25c3af7ef816b55f1b35bd9f
nickola-team <CoreDev0703@gmail.com>
2024-08-16 22:41:02 +0900

    hot chilli test 25 light

--- app/Games/_3hotchilliesbng/SlotSettings.php
+++ app/Games/_3hotchilliesbng/SlotSettings.php
@@ -718,8 +718,10 @@ namespace VanguardLTE\Games\_3hotchilliesbng
                             }
                             else
                             {
-                                Log::info('limitodd %%%% ' . $limitOdd . ' stack ' . $stacks->where('odd', '<=', $limitOdd)->get());
-                                $stacks = $stacks->where('odd', '<=', $limitOdd)->get();
+                                Log::info('limitodd %%%% ' . $limitOdd . ' stack ' . $stacks->where('odd', '<=', $limitOdd)->count());
+                                $stacks = $stacks->where('odd', '<=', $limitOdd);
+                                Log::info('text %%%% ' . $stacks->count());
+                                $stacks->get();
                                 // $stacks = $stacks->where('odd', '<=', 169)->get();
                                 Log::info('stacks %%%%');
                             }
