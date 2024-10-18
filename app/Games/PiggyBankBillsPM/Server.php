f5bcbf4cbd64c47bb45499766bcc7e7ea2526372
wangpingping5 <dev.wangping@gmail.com>
2024-10-18 19:56:33 +0900

    update pp roundid

--- app/Games/PiggyBankBillsPM/Server.php
+++ app/Games/PiggyBankBillsPM/Server.php
@@ -277,7 +277,7 @@ namespace VanguardLTE\Games\PiggyBankBillsPM
                     $slotSettings->SetGameData($slotSettings->slotId . 'TotalSpinCount', 0);
                     $roundstr = sprintf('%.4f', microtime(TRUE));
                     $roundstr = str_replace('.', '', $roundstr);
-                    $roundstr = '4174458' . substr($roundstr, 4, 10);
+                    $roundstr = '6074458' . substr($roundstr, 4, 10);
                     $slotSettings->SetGameData($slotSettings->slotId . 'RoundID', $roundstr);   // Round ID Generation
                     $leftFreeGames = 0;
                 }
