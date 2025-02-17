c5ac45313686e58f8d123d2c3431d7d2b0d708b8
light <CoreDev0703@gmail.com>
2025-02-17 16:28:35 +0900

    frontend change some light

--- resources/views/frontend/boss/layouts/partials/notice.blade.php
+++ resources/views/frontend/boss/layouts/partials/notice.blade.php
@@ -14,6 +14,7 @@
                 </th>
             </tr>
             @foreach ($noticelist as $ntc)
+                @if($ntc->popup == 'general')
                 <tr role="button" class="ng-scope">
                     <td class="width30" style="width: 134px;">
                     <span class="ng-binding">{{date('Y-m-d', strtotime($ntc->date_time))}}</span>&nbsp;
@@ -22,14 +23,15 @@
                     <span onclick="navClick('msg-popup');setTab('notice-set','#msg-popup &gt; div.ngdialog-content &gt; div.ngdialog-customer-page.ngdialog-main-default-page.ng-scope &gt; ul &gt; li:nth-child(1)');getNotice('notice{{$ntc->id}}');" class="ng-binding">{{$ntc->title}}</span>
                     </td>
                 </tr>
+                @endif
             @endforeach
             </tbody>
         </table>
-        <div class="transaction-table">
+        {{-- <div class="transaction-table">
             <div class="table-heading" style="text-align: left;display: block;">
             <span class="transaction-title active">실시간 입금 현황</span>
             </div>
-            <ul class="transaction-list ng-scope" id="deposit-ticker"></ul>
+            <ul class="transaction-list ng-scope" id="deposit-ticker" ></ul>
 
         </div>
         <div class="transaction-table">
@@ -38,7 +40,51 @@
             </div>
             <ul class="transaction-list" id="withdraw-ticker"></ul>
         </div>
+        <div class="clearfix"></div> --}}
+
+        <div class="transaction-table">
+            <div class="table-heading"><span>실시간 입금 현황</span></div>
+            <ul class=" list-unstyled transaction-list"  style="width: 414px;" id="deposit-ticker">
+                
+            </ul>
+        </div>
+        <div class="transaction-table">
+            <div class="table-heading"><span>실시간 출금 현황</span></div>
+            <ul class=" list-unstyled transaction-list"  style="width: 414px;" id="withdraw-ticker">
+            </ul>
+        </div>
         <div class="clearfix"></div>
         </div>
     </div>
     </transaction-page>
+
+    <script>
+        setTimeout(function () {
+          $('#notice-ticker').newsTicker({
+            row_height: 14.5,
+            max_rows: 11,
+            duration: 2000,
+            autostart:0
+          });
+          $('#deposit-ticker').newsTicker({
+            row_height: 32,
+            max_rows: 5,
+            direction: 'up',
+            autostart: 1,
+            pauseOnHover: 1,
+            delay : 10000
+          });
+        });
+        setTimeout(function () {
+          $('#withdraw-ticker').newsTicker({
+            row_height: 32,
+            max_rows: 5,
+            direction: 'up',
+            autostart: 1,
+            pauseOnHover: 1,
+            delay : 10000
+          });
+        },500);
+        depositRealtime();
+      withdrawRealtime();
+      </script>
